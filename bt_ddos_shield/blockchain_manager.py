3eb87ff6b21a16443cc8aca706a5e2dd26e8ea56
Tomasz Żyźniewski <zyzniewski@gmail.com>
2024-12-20 13:55:32 +0100

    feat: Bittensor BlockchainManager using commitments of knowledge

--- bt_ddos_shield/blockchain_manager.py
+++ bt_ddos_shield/blockchain_manager.py
@@ -1,7 +1,18 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
-from bt_ddos_shield.address import Address, AbstractAddressSerializer, AddressDeserializationException
+import bittensor
+import bittensor_wallet
+from bittensor.core.extrinsics.serving import (
+    get_metadata,
+    publish_metadata,
+)
+
+from bt_ddos_shield.address import (
+    AbstractAddressSerializer,
+    Address,
+    AddressDeserializationException,
+)
 from bt_ddos_shield.utils import Hotkey
 
 
@@ -50,3 +61,56 @@ class AbstractBlockchainManager(ABC):
         Get data from blockchain for given user identified by hotkey or None if not found.
         """
         pass
+
+
+class BittensorBlockchainManager(AbstractBlockchainManager):
+    """
+    Bittensor BlockchainManager implementation using commitments of knowledge as storage.
+    """
+
+    def __init__(
+        self,
+        address_serializer: AbstractAddressSerializer,
+        subtensor: bittensor.Subtensor,
+        wallet: bittensor_wallet.Wallet,
+        netuid: int,
+    ):
+        super().__init__(address_serializer)
+
+        self.subtensor = subtensor
+        self.wallet = wallet
+        self.netuid = netuid
+
+    def get(self, hotkey: Hotkey) -> Optional[bytes]:
+        """
+        Get data from blockchain for given user identified by hotkey or None if not found.
+        """
+
+        metadata = get_metadata(
+            self.subtensor,
+            self.netuid,
+            hotkey,
+        )
+
+        try:
+            commitment = metadata["info"]["fields"][0]  # type: ignore
+            hex_data = commitment[list(commitment.keys())[0]][2:]  # type: ignore
+            return bytes.fromhex(hex_data)
+        except TypeError:
+            return None
+
+    def put(self, hotkey: Hotkey, data: bytes):
+        """
+        Put data to blockchain for given user identified by hotkey.
+        """
+
+        if hotkey != self.wallet.hotkey.ss58_address:
+            raise ValueError("Hotkey not in Wallet")
+
+        publish_metadata(
+            self.subtensor,
+            self.wallet,
+            self.netuid,
+            f"Raw{len(data)}",
+            data,
+        )
--- tests/test_blockchain_manager.py
+++ tests/test_blockchain_manager.py
@@ -1,7 +1,13 @@
+import unittest.mock
 from typing import Optional
 
+import pytest
+
 from bt_ddos_shield.address import DefaultAddressSerializer
-from bt_ddos_shield.blockchain_manager import AbstractBlockchainManager
+from bt_ddos_shield.blockchain_manager import (
+    AbstractBlockchainManager,
+    BittensorBlockchainManager,
+)
 from bt_ddos_shield.utils import Hotkey
 
 
@@ -22,8 +28,109 @@ class MemoryBlockchainManager(AbstractBlockchainManager):
         return self.known_data.get(hotkey)
 
 
-class TestBlockchainManager:
+class TestBittensorBlockchainManager:
     """
     Test suite for the blockchain manager.
     """
-    pass
+
+    def test_get(self):
+        hotkey = "5EU2xVWC7qffsUNGtvakp5WCj7WGJMPkwG1dsm3qnU2Kqvee"
+
+        mock_subtensor = unittest.mock.MagicMock()
+        mock_substrate = mock_subtensor.substrate.__enter__.return_value
+        mock_substrate.query.return_value = unittest.mock.Mock(
+            value={
+                "info": {
+                    "fields": None,
+                },
+            },
+        )
+
+        manager = BittensorBlockchainManager(
+            address_serializer=DefaultAddressSerializer(),
+            subtensor=mock_subtensor,
+            wallet=unittest.mock.Mock(),
+            netuid=1,
+        )
+
+        assert manager.get(hotkey) is None
+
+        mock_substrate.query.assert_called_once_with(
+            module="Commitments",
+            storage_function="CommitmentOf",
+            params=[1, "5EU2xVWC7qffsUNGtvakp5WCj7WGJMPkwG1dsm3qnU2Kqvee"],
+            block_hash=None,
+        )
+
+        mock_substrate.query.reset_mock()
+        mock_substrate.query.return_value = unittest.mock.Mock(
+            value={
+                "info": {
+                    "fields": [
+                        {
+                            "Raw4": "0x64617461",
+                        },
+                    ],
+                },
+            },
+        )
+
+        assert manager.get(hotkey) == b"data"
+
+    def test_put(self):
+        hotkey = "5EU2xVWC7qffsUNGtvakp5WCj7WGJMPkwG1dsm3qnU2Kqvee"
+
+        mock_subtensor = unittest.mock.MagicMock()
+        mock_substrate = mock_subtensor.substrate.__enter__.return_value
+
+        mock_wallet = unittest.mock.Mock()
+        mock_wallet.hotkey.ss58_address = hotkey
+
+        manager = BittensorBlockchainManager(
+            address_serializer=DefaultAddressSerializer(),
+            subtensor=mock_subtensor,
+            wallet=mock_wallet,
+            netuid=1,
+        )
+
+        manager.put(hotkey, b"data")
+
+        mock_substrate.compose_call.assert_called_once_with(
+            call_module="Commitments",
+            call_function="set_commitment",
+            call_params={
+                "netuid": 1,
+                "info": {
+                    "fields": [
+                        [
+                            {
+                                "Raw4": b"data",
+                            },
+                        ],
+                    ],
+                },
+            },
+        )
+        mock_substrate.create_signed_extrinsic.assert_called_once_with(
+            call=mock_substrate.compose_call.return_value,
+            keypair=mock_wallet.hotkey,
+        )
+        mock_subtensor.substrate.submit_extrinsic.assert_called_once_with(
+            mock_substrate.create_signed_extrinsic.return_value,
+            wait_for_inclusion=False,
+            wait_for_finalization=True,
+        )
+
+    def test_put_not_own_hotkey(self):
+        mock_wallet = unittest.mock.Mock()
+        mock_wallet.hotkey.ss58_address = "MyHotkey"
+
+        manager = BittensorBlockchainManager(
+            address_serializer=DefaultAddressSerializer(),
+            subtensor=unittest.mock.MagicMock(),
+            wallet=mock_wallet,
+            netuid=1,
+        )
+
+        with pytest.raises(ValueError):
+            manager.put("SomeoneHotkey", b"data")
